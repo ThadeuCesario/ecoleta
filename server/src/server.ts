4ba1114ade608c3ff5ce39a936067972354cceaf
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-06 12:18:55 -0300

    cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -71,11 +71,13 @@
  */
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import path from 'path';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
