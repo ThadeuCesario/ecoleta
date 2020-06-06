a444fba4e0642399ecad3064eba04d5267b1a13c
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-05 23:58:01 -0300

    Clear workspace

--- server/src/server.ts
+++ server/src/server.ts
@@ -86,31 +86,12 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
+app.get('/', (request, response) => {
     const search = String(request.query.search);
 
     const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
 
     response.json(filteredUsers);
 });
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id); //O 'id' é totalmente customizado, podemos colocar qualquer nome
-    const user = users[id];
-    return response.json(user);
-})
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    console.log(data);
-
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-
-    return response.json(user);
-})
 
 app.listen(3333);
