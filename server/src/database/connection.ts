704528cacda82c89e236a7f0dd965d8b5cb1bba7
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-06 01:26:19 -0300

    database and all migrations

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -29,7 +29,8 @@ const connection = knex({
     client: 'sqlite3',
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite'),
-    }
+    },
+    useNullAsDefault: true,
 });
 
 export default connection;
--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -1,3 +1,31 @@
-export async function up(){}
+/* Comentários:
+ * As migrations são executadas de forma sequencial. Por isso, precisamos que seja ordenada as migrations
+ * de forma que não gere nenhum tipo de problema.
+ *
+ * Sempre precisaremos ter duas função, a função up() e a função down().
+ * Basicamente a função up() realiza as alterações que desejamos, e a função down() é executada em caso
+ * de problema, para desfazer as alterações que tentaram ser realizadas.
+ * Então se no método up() adicionamos um campo em uma tabela, no método down() removemos esse campo
+ * da tabela.
+ *
+ * --------------
+ * Typescript
+ * Vamos importar o Knex com letra maiúscula, lembrando que typescript os tipos geralmente
+ * são referenciados por letra maiúscula.
+ * É muito interessante no typescript, basicamente dizer qual a tipagem de uma variável a automaticamente
+ * ganhar toda a inteligência e atributos que são herdados.
+ */
 
-export async function down(){}
+import Knex from 'knex'
+
+export async function up(knex: Knex){
+    return knex.schema.createTable('items', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    })
+}
+
+export async function down(knex: Knex){
+    return knex.schema.dropTable('items');
+}
