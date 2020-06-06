8da4ae410160e85480c98e7ae695a488f50574ce
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-06 16:57:36 -0300

    CSS Page Home

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,13 +2,15 @@
 import React from 'react';
 
 /* Import Components */
+import Home from "./pages/Home";
+
 /* Import CSS */
 import './App.css';
 
 function App() {
   return (
       <>
-        <h1>Hello World</h1>
+        <Home />
       </>
   );
 }
---
+++ front/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Home:React.FC = props => {
+    return(
+        <h1>Home Component</h1>
+    );
+}
+
+export default Home;
