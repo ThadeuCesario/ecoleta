2ae462f37468d658491c01c8e369a39b75599957
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-13 14:40:25 -0300

    routes and resources

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+/**
+ * Rota: Endereço completo de nossa requisição. 
+ * Recurso: Qual entendidade estamos acessando do sistema.
+ * 
+ */
+
 app.get('/users', (request, response) => {
     console.log("Listagem de usuários");
 
