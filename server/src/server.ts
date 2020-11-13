10324d73cbc5f70b064cbe9f9945d956d373688f
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-13 14:09:30 -0300

    Return users

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,12 @@ const app = express();
 app.get('/users', (request, response) => {
     console.log("Listagem de usuários");
 
-    response.send("Hello World!");
+    response.json([
+        "Thadeu",
+        "Karina",
+        "Gustavo",
+        "Katharina",
+    ]);
 });
 
 app.listen(3333);
