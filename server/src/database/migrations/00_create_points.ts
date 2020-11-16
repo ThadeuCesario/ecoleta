4a61456a4b7a2e79592b8636ece087ceb9adf2cb
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-16 19:15:52 -0300

    implementing our migrations

---
+++ server/src/database/migrations/00_create_points.ts
@@ -0,0 +1,16 @@
+/**
+ * Veja que estou utilizando letra maiúscula, isso porque quando estou
+ * me referindo ao tipo da variável que estou importando e não a própria
+ * variável em si. 
+ * Então utilizamos letra maiúscula.
+ */
+
+import Knex from 'knex';
+
+export async function up(knex) {
+    // Criar a tabela
+}
+
+export async function down(knex) {
+    // Deletar a tabela - o método down reverte o método down
+}
