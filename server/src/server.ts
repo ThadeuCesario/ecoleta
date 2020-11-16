619f65c0fce4a73fd24a918759ed32d2a223a154
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-16 13:48:14 -0300

    http methods

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,14 +3,18 @@ import express from 'express';
 const app = express();
 
 /**
- * Rota: Endereço completo de nossa requisição. 
- * Recurso: Qual entendidade estamos acessando do sistema.
+ * Rota: Endereço completo de nossa requisição
+ * Recurso: Qual entendidade estamos acessando do sistema
  * 
+ * Requisições HTTP 
+ * GET: Buscar uma ou mais informações do back-end
+ * POST: Criar uma nova informação no back-end
+ * PUT: Atualizar uma informação existente no back-end
+ * DELETE: Deletar uma informação existente no back-end 
  */
 
 app.get('/users', (request, response) => {
     console.log("Listagem de usuários");
-
     response.json([
         "Thadeu",
         "Karina",
