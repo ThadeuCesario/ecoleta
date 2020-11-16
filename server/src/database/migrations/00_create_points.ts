73eeacb0617469ec3779671172378669c0ed4b77
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-16 19:36:07 -0300

    migration - 00_create_points

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -1,3 +1,4 @@
+import { knex } from 'knex';
 /**
  * Veja que estou utilizando letra maiúscula, isso porque quando estou
  * me referindo ao tipo da variável que estou importando e não a própria
@@ -9,8 +10,20 @@ import Knex from 'knex';
 
 export async function up(knex: Knex) {
     // Criar a tabela
+    return knex.schema.createTable('points', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.decimal('latitude').notNullable();
+        table.decimal('longitude').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
+    });
 }
 
-export async function down(knex) {
+export async function down(knex: Knex) {
     // Deletar a tabela - o método down reverte o método down
+    return knex.schema.dropTable('point');
 }
