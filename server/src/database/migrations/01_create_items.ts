bddfdee081c40993656ba4155a84347bbf74ec86
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-16 20:04:30 -0300

    finishing our migrations

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -1,9 +1,20 @@
+/**
+ * Veja que estou utilizando letra maiúscula, isso porque quando estou
+ * me referindo ao tipo da variável que estou importando e não a própria
+ * variável em si. 
+ * Então utilizamos letra maiúscula.
+ */
+
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-
+    return knex.schema.createTable('items', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    });
 }
 
 export async function down(knex: Knex) {
-
+    return knex.schema.dropTable('point');
 }
--- server/src/database/migrations/02_create_point_items.ts
+++ server/src/database/migrations/02_create_point_items.ts
@@ -1,9 +1,22 @@
+/**
+ * Veja que estou utilizando letra maiúscula, isso porque quando estou
+ * me referindo ao tipo da variável que estou importando e não a própria
+ * variável em si. 
+ * Então utilizamos letra maiúscula.
+ */
+
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-
+    // Criar a tabela
+    return knex.schema.createTable('point_items', table => {
+        table.increments('id').primary();
+        table.integer('point_id').notNullable().references('id').inTable('points'); //Criando chave estrangeira
+        table.integer('item_id').notNullable().references('id').inTable('items');
+    });
 }
 
 export async function down(knex: Knex) {
-
+    // Deletar a tabela - o método down reverte o método down
+    return knex.schema.dropTable('point_items');
 }
