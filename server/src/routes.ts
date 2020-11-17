e3dab03aec5f46f49e73ef76f7c6810caba664e2
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-17 15:00:43 -0300

    Creating controllers, routes, server...

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,63 +1,20 @@
 import express from 'express';
 import knex from './database/connection';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 /* Dessa forma, conseguiremos desacoplar nossas rotas do arquivo principal. 
  * Toda vez que realizarmos uma query no banco de dados, precisamos utilizar o await.
  * É um processo que precisa ser sincronizado.
  */
+
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-    const items = await knex('items').select('*');
+routes.get('/items', itemsController.create);
 
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}` 
-        };
-    })
-    return response.json(serializedItems);
-});
-
-routes.post('/points', async (request, response) => {
-    const {
-        name, 
-        email,
-        whatsapp, 
-        latitude, 
-        longitude,
-        city, 
-        uf,
-        items
-    } = request.body;
-
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name, 
-        email,
-        whatsapp, 
-        latitude, 
-        longitude,
-        city, 
-        uf
-    });
-
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id,
-        }
-    });
-
-    await trx('point_items').insert(pointItems);
-
-    return response.json({sucess: true});
-});
+routes.post('/points', pointsController.create);
 
 
 export default routes;
