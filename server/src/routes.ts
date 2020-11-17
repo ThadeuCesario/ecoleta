b2a44fe10ade48a33f28f39fbae96ebca0683f68
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-17 11:10:41 -0300

    query database

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import knex from './database/connection';
 
 /* Dessa forma, conseguiremos desacoplar nossas rotas do arquivo principal. 
- * 
+ * Toda vez que realizarmos uma query no banco de dados, precisamos utilizar o await.
+ * É um processo que precisa ser sincronizado.
  */
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
+routes.get('/', async (request, response) => {
+    const items = await knex('items').select('*');
     return response.json({message: 'Hello World'});
 });
 
