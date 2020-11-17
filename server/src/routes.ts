0963eca58b8c3450d57a2a867dcf6bedf56e10a6
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-17 11:27:02 -0300

    items serialized

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,7 +9,13 @@ const routes = express.Router();
 
 routes.get('/items', async (request, response) => {
     const items = await knex('items').select('*');
-    return response.json(items);
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}` 
+        };
+    })
+    return response.json(serializedItems);
 });
 
 
