6f07f03054a6caab8416e04bd2181430dcf50cbf
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-17 14:08:01 -0300

    back-end database, migrations, transactions, knex

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,13 +9,54 @@ const routes = express.Router();
 
 routes.get('/items', async (request, response) => {
     const items = await knex('items').select('*');
+
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}` 
         };
     })
     return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+    const {
+        name, 
+        email,
+        whatsapp, 
+        latitude, 
+        longitude,
+        city, 
+        uf,
+        items
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name, 
+        email,
+        whatsapp, 
+        latitude, 
+        longitude,
+        city, 
+        uf
+    });
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id,
+        }
+    });
+
+    await trx('point_items').insert(pointItems);
+
+    return response.json({sucess: true});
 });
 
 
