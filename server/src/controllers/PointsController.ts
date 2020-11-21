c8eb48cb2fa1f411982a13cca2e1a2be29074acf
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-21 04:26:06 -0300

    finish backend

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -51,7 +51,7 @@ class PointsController {
         const trx = await knex.transaction();
 
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1588176185841-91265480d77c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name, 
             email,
             whatsapp,
