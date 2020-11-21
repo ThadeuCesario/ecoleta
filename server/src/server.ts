b61b604fe53bd956d004b51d7202f9359b2f419e
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-21 00:10:05 -0300

    setting the routes file

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,5 @@ import routes from './routes';
 
 const app = express();
 app.use(express.json());
-
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello World'});
-});
-
+app.use(routes);
 app.listen(3333);
