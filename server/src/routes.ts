dbfada8b18304ff20e0e27c2bc527d48e8ed0e5c
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-21 03:40:27 -0300

    fixing backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'Hello World'});
-});
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
