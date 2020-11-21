52eab68150a1d7c569890b1b0a45a2e14fa103ff
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-20 23:22:51 -0300

    testing back-end server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,36 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
+app.use(express.json());
+
+const users = [
+    'Diego',
+    'Cleiton',
+    'Thadeu'
+];
 
 app.get('/users', (request, response) => {
-    console.log("Listagem de usuários");
-
-    return response.json([
-        'Diego',
-        'Cleiton',
-        'Thadeu'
-    ]);
+    const {search} = request.query;
+    
+    const filteredUsers = users.filter(user => user.includes(String(search)))
+    return response.json(filteredUsers);
 });
+
+app.get('/users/:id', (request, response) => {
+    const {id} = request.params;
+    const user = users[Number(id)];
+    return response.json(user);
+}); 
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email,
+    };
+
+    return response.json(user);
+})
 
 app.listen(3333);
