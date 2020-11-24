30b65c52e218d6749ca63b02d00a6d03fe9fe9cf
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-23 21:25:08 -0300

    ecoleta frontend

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Header.tsx';
+
+function App() {
+  return (
+    <div>
+      <Header title="Hello World" />
+      <h1>Conteúdo do aplicação</h1>
+    </div>
+    
+  );
+}
+
+export default App;
