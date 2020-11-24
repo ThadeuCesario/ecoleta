c7493e3a002d66fd8017042b318a3c36bf3ef01e
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-24 14:51:00 -0300

    creating new project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <h1>Conteúdo do aplicação</h1>
+    <div className="App">
+      Opa
     </div>
-    
   );
 }
 
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.tsx';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,4 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
