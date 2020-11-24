ab91da6f853c2e702a633676038e698ec81f11d7
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-24 14:59:03 -0300

    implementing header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      Opa
-    </div>
+    <Header />
   );
 }
 
