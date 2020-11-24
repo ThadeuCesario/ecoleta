3553a34a5b3bdaaa5fef8156ba2ed973c85afb82
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-24 16:11:24 -0300

    Implementing a simple counter

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <Header title='Hello World'/>
+    <>
+      <Header title='Hello World'/>
+      <span>{counter}</span>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </>
   );
 }
 
