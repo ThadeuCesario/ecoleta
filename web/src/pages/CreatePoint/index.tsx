d56b040b804303007b7da06ea2a261897de31c67
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-11-24 22:29:01 -0300

    CreatePoint - index

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -14,6 +14,66 @@ const CreatePoint = () => {
           Voltar para home
         </Link>
       </header>
+      <form>
+        <h1>Cadastro do <br/> ponto de coleta</h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+          <div className="field">
+            <label htmlFor="name">
+              Nome da entidade
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+            />
+          </div>
+
+          <div className="field-group">
+          <div className="field">
+            <label htmlFor="email">
+              E-mail
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="whatsapp">
+              Whatsapp
+            </label>
+            <input
+              type="text"
+              name="whatsapp"
+              id="whatsapp"
+            />
+          </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+          </legend>
+          <div className="field">
+
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Itens de coleta</h2>
+          </legend>
+          <div className="field">
+
+          </div>
+        </fieldset>
+      </form>
     </div>
   )
 }
